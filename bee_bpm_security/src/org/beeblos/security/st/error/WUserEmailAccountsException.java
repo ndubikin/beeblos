23ab1c5ef8f0e1e0b15876e6a5ecfe79ca54af1b
dmuleiro <dmuleiro@2fb576a0-0bdd-42ec-9e08-7f43f70b2762>
2012-02-23 10:38:55 +0000

    - añadida la funcionalidad de añadir emails a un wuserdef cualquiera mediante un CRUD y tambien la pestaña en "My account" para añadir/editar/eliminar las cuentas propias del usuario logueado

---
+++ bee_bpm_security/src/org/beeblos/security/st/error/WUserEmailAccountsException.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.beeblos.security.st.error;
+
+public class WUserEmailAccountsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 24652353247973549L;
+
+	/**
+	 * 
+	 */
+	public WUserEmailAccountsException() {
+		
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public WUserEmailAccountsException(String arg0) {
+		super(arg0);
+		
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public WUserEmailAccountsException(Throwable arg0) {
+		super(arg0);
+		
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public WUserEmailAccountsException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		
+	}
+
+}
