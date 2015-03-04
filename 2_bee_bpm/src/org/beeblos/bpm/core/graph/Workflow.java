2b3e0d9111e6fd4fcbde72d46519a0097721fc31
ndubikin <ndubikin@2fb576a0-0bdd-42ec-9e08-7f43f70b2762>
2015-03-04 15:28:53 +0000

    Terminado el tema de ligar métodos Externos a las sequences en el Mapa.
    WExternalMedhods en WStepSequenceDef

--- 2_bee_bpm/src/org/beeblos/bpm/core/graph/Workflow.java
+++ 2_bee_bpm/src/org/beeblos/bpm/core/graph/Workflow.java
@@ -1,6 +1,13 @@
 package org.beeblos.bpm.core.graph;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+import org.beeblos.bpm.core.model.WExternalMethod;
 
 public class Workflow {
 
@@ -8,6 +15,7 @@ public class Workflow {
 	private String id;
 	private String spId;
 	private String label;
+	private Set<WExternalMethod> method;
 
 	
 	public Workflow() {
@@ -20,6 +28,16 @@ public class Workflow {
 		this.spId = spId;
 		this.label = label;
 	}
+
+	public Workflow(String description, String id, String spId, String label,
+			Set<WExternalMethod> method) {
+		this.description = description;
+		this.id = id;
+		this.spId = spId;
+		this.label = label;
+		this.method = method;
+	}
+
 	/**
 	 * @return the description
 	 */
@@ -72,16 +90,46 @@ public class Workflow {
 	public void setLabel(String label) {
 		this.label = label;
 	}
+
+	private String toString(Collection<?> collection, int maxLen) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[");
+		int i = 0;
+		for (Iterator<?> iterator = collection.iterator(); iterator.hasNext()
+				&& i < maxLen; i++) {
+			if (i > 0)
+				builder.append(", ");
+			builder.append(iterator.next());
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+	/**
+	 * @return the method
+	 */
+	@XmlElement(name="method")
+	public Set<WExternalMethod> getMethod() {
+		return method;
+	}
+	/**
+	 * @param method the method to set
+	 */
+	public void setMethod(Set<WExternalMethod> method) {
+		this.method = method;
+	}
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
+		final int maxLen = 2;
 		return "Workflow ["
 				+ (description != null ? "description=" + description + ", "
 						: "") + (id != null ? "id=" + id + ", " : "")
 				+ (spId != null ? "spId=" + spId + ", " : "")
-				+ (label != null ? "label=" + label : "") + "]";
+				+ (label != null ? "label=" + label + ", " : "")
+				+ (method != null ? "method=" + toString(method, maxLen) : "")
+				+ "]";
 	}
 	/* (non-Javadoc)
 	 * @see java.lang.Object#hashCode()
@@ -94,6 +142,7 @@ public class Workflow {
 				+ ((description == null) ? 0 : description.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		result = prime * result + ((method == null) ? 0 : method.hashCode());
 		result = prime * result + ((spId == null) ? 0 : spId.hashCode());
 		return result;
 	}
@@ -123,6 +172,11 @@ public class Workflow {
 			if (other.label != null)
 				return false;
 		} else if (!label.equals(other.label))
+			return false;
+		if (method == null) {
+			if (other.method != null)
+				return false;
+		} else if (!method.equals(other.method))
 			return false;
 		if (spId == null) {
 			if (other.spId != null)
